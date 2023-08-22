0fdf502fc5036783fa0a1b1ae3bb88bae6cf750d
fabberr <fabriciomilanez@protonmail.com>
2023-08-22 03:10:03 +0000

    Add CRUD operations for `routes` resource
    [feat] Added methods for the `routes` resouce:
      - POST /routes        Create new Route
      - GET /routes         Find all Routes.
      - GET /routes/:id     Find one Route.
      - PATCH /routes/:id   Update Route.
      - DELETE /routes/:id  Delete Route.

--- src/routes/dto/update-route.dto.ts
+++ src/routes/dto/update-route.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateRouteDto } from './create-route.dto';
 
-export class UpdateRouteDto extends PartialType(CreateRouteDto) {}
+export class UpdateRouteDto extends PartialType(CreateRouteDto) {
+    name: string;
+    source_id: string;
+    destination_id: string;
+}
--- src/routes/routes.service.ts
+++ src/routes/routes.service.ts
@@ -1,26 +1,76 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRouteDto } from './dto/create-route.dto';
 import { UpdateRouteDto } from './dto/update-route.dto';
+import { PrismaService } from 'src/prisma/prisma/prisma.service';
 
 @Injectable()
 export class RoutesService {
+  constructor(private prismaService: PrismaService) { }
+
   create(createRouteDto: CreateRouteDto) {
-    return 'This action adds a new route';
+    return this.prismaService.route.create({
+      data: {
+        name: createRouteDto.name,
+        source: {
+          name: createRouteDto.source_id,
+          location: {
+            lat: 0,
+            lng: 0
+          }
+        },
+        destination: {
+          name: createRouteDto.destination_id,
+          location: {
+            lat: 0,
+            lng: 0
+          }
+        },
+        distance: 0,
+        duration: 0,
+        directions: '{}'
+      }
+    });
   }
 
   findAll() {
-    return `This action returns all routes`;
+    return this.prismaService.route.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} route`;
+  findOne(id: string) {
+    return this.prismaService.route.findUnique({
+      where: { id: id }
+    });
   }
 
-  update(id: number, updateRouteDto: UpdateRouteDto) {
-    return `This action updates a #${id} route`;
+  update(id: string, updateRouteDto: UpdateRouteDto) {
+    return this.prismaService.route.update({
+      where: { id: id },
+      data: {
+        name: updateRouteDto.name,
+        source: {
+          name: updateRouteDto.source_id,
+          location: {
+            lat: 0,
+            lng: 0
+          }
+        },
+        destination: {
+          name: updateRouteDto.destination_id,
+          location: {
+            lat: 0,
+            lng: 0
+          }
+        },
+        distance: 0,
+        duration: 0,
+        directions: '{}'
+      }
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} route`;
+  remove(id: string) {
+    return this.prismaService.route.delete({
+      where: { id: id },
+    });
   }
 }
